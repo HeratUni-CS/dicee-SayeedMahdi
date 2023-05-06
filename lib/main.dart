7eec8763140379e98eeea80dc32c53f1d0a8860f
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2023-05-06 22:56:45 +0430

    ready to use

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(
+  return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.black,
         appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text('Dice'),
+          title: Text('GFG | Dice'),
+          backgroundColor: Color.fromRGBO(15, 157, 88, 1),
         ),
-        body: MyApp(),
+        body: Myapp(),
       ),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class Myapp extends StatefulWidget {
+  @override
+  _RandState createState() => _RandState();
+}
+
+class _RandState extends State<Myapp> {
+  int number = 1;
+  void update() {
+    setState(() {
+      //Random.nextInt(n) returns random integer from 0 to n-1
+      number = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Expanded(
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: FlatButton(
+            child: Image.asset('images/dice$number.png'),
+            onPressed: () {
+              update();
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
